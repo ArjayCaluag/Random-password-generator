6ae4da48b31461b10ce8f6ffbbf341e9b78dea88
Ron-Arjay Caluag <arjaycaluag@gmail.com>
2020-12-18 20:14:16 -0800

    Add if statements so that use must input specific character length and at least one type

--- Develop/script.js
+++ Develop/script.js
@@ -23,15 +23,27 @@ function generatePassword(){
           var lowerCaseArray = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
           var upperCaseArray = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
           var numberArray = [0,1,2,3,4,5,6,7,8,9]
-          var specialCharacterArray =  ['!',',','@','#','$','%','^','&','*']
+          var specialCharacterArray =  ['!',',','@','#','$','%','^','&','*','(',')','-','_']
+
+        // Must select between 8-128 characters, otherwise stops function
+        var length = prompt('Password length can be between 8-128 chracters, how many characters would you like it to be?');
+          if (length < 8 || length > 128) {
+          alert("Password length must be 8-128 characters");
+          return;
+          }
           
           // confirms used to create a popup prompting users to select OK(true) or Cancel (false)
-          var length = prompt('How many characters do you want your password to be?');
+        
           var shouldIncludeLower = confirm('Do you want password to contain lowercase characters?');
           var shouldIncludeUpper = confirm('Do you want password to contain uppercase characters?');
           var shouldIncludeNumbers = confirm('Do you want password to contain numbers?');
           var shouldIncludeSpecial = confirm('Do you want password to contain special characters?');
-              
+
+          // At least one character type must be selected, otherwise stops function
+          if(shouldIncludeUpper!= true && shouldIncludeLower !=true && shouldIncludeNumbers !=true && shouldIncludeSpecial !=true){
+            alert("Atleast one selector type must be used");
+            return;
+          }
 
           //empty array to concat all of userinputs confirmed options to
          var userInput = []
@@ -41,19 +53,19 @@ function generatePassword(){
         
           // concat() method used to combine arrays and returns a new array
           //if user selects include lower case letters, add lowerCaseArry to userInput array
-          if (shouldIncludeLower = true) {
+          if (shouldIncludeLower == true) {
             userInput = userInput.concat(lowerCaseArray);
           }
           //if user selects upper case letters, add upperCaseArry to userInput array
-          if (shouldIncludeUpper = true) {
+          if (shouldIncludeUpper == true) {
             userInput = userInput.concat(upperCaseArray);
           }
           //if user selects include lower case numbers, add numbersArray to userInput array
-          if (shouldIncludeNumbers = true) {
+          if (shouldIncludeNumbers == true) {
             userInput = userInput.concat(numberArray);
           }
           //if user selects include special case characters, add specialCharacterArray to userInput array
-          if (shouldIncludeSpecial = true) {
+          if (shouldIncludeSpecial == true) {
             userInput = userInput.concat(specialCharacterArray);
           }
         
@@ -67,5 +79,6 @@ function generatePassword(){
               randomizedPassword.push(temporary);
           }
 
-          console.log(randomizedPassword)
+          // joins the array and eliminates the comma separating them
+          return randomizedPassword.join("");
 }
