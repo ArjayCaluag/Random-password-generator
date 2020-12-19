e9000d5a936eecb9b5fdb99853bd9b4ef01c25f6
Ron-Arjay Caluag <arjaycaluag@gmail.com>
2020-12-18 18:58:19 -0800

    Add for loop to generate random password and empty array to store password

--- Develop/script.js
+++ Develop/script.js
@@ -8,9 +8,6 @@ function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector('#password');
 
-
-
-
   passwordText.value = password;
 
 }
@@ -20,6 +17,8 @@ generateBtn.addEventListener('click', writePassword);
 
 function generatePassword(){
 
+
+
         // Possible password options
           var lowerCaseArray = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
           var upperCaseArray = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
@@ -27,31 +26,46 @@ function generatePassword(){
           var specialCharacterArray =  ['!',',','@','#','$','%','^','&','*']
           
           // confirms used to create a popup prompting users to select OK(true) or Cancel (false)
+          var length = prompt('How many characters do you want your password to be?');
           var shouldIncludeLower = confirm('Do you want password to contain lowercase characters?');
           var shouldIncludeUpper = confirm('Do you want password to contain uppercase characters?');
           var shouldIncludeNumbers = confirm('Do you want password to contain numbers?');
           var shouldIncludeSpecial = confirm('Do you want password to contain special characters?');
+              
 
-          //empty array to concat userInput data to
-          var userInput = []
+          //empty array to concat all of userinputs confirmed options to
+         var userInput = []
 
+         // empty array for random to be stored
+         var randomizedPassword = []
+        
           // concat() method used to combine arrays and returns a new array
-          //if user selects include lower case letters, add lowerCaseArry to new array
+          //if user selects include lower case letters, add lowerCaseArry to userInput array
           if (shouldIncludeLower = true) {
-          userInput = userInput.concat(lowerCaseArray);
+            userInput = userInput.concat(lowerCaseArray);
           }
-          //if user selects upper case letters, add upperCaseArry to new array
+          //if user selects upper case letters, add upperCaseArry to userInput array
           if (shouldIncludeUpper = true) {
-          userInput = userInput.concat(upperCaseArray);
-          } 
-          //if user selects include lower case numbers, add numbersArray to new array
-          if (shouldIncludeNumbers = true) {
-          userInput = userInput.concat(numberArray);
+            userInput = userInput.concat(upperCaseArray);
           }
-          //if user selects include special case characters, add specialCharacterArray to new array
+          //if user selects include lower case numbers, add numbersArray to userInput array
+          if (shouldIncludeNumbers = true) {
+            userInput = userInput.concat(numberArray);
+          }
+          //if user selects include special case characters, add specialCharacterArray to userInput array
           if (shouldIncludeSpecial = true) {
-          userInput = userInput.concat(specialCharacterArray);
-          }    
-          // logs all possible options based off of userinput
-          console.log(userInput);
-        }        
+            userInput = userInput.concat(specialCharacterArray);
+          }
+        
+          // For loop to run the code over again until the amount the user inputs to achieve desired password length.
+          // randomNumber uses math.floor to round any possible decimal index that math.random() * userInput.length may push out. -1 to take into account that index reference always starts at 0
+          // temporary is created to temporarily randomized array
+          // .push is used to add temporary onto the empty randomizedPassword array
+          for (var i = 0; i < length; i++){
+              var randomNumber = Math.floor(Math.random() * userInput.length -1)
+              var temporary = userInput[randomNumber]
+              randomizedPassword.push(temporary);
+          }
+
+          console.log(randomizedPassword)
+}
